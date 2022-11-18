a427eb7a2bd7c9daefcfd5a47d4aa05cdd2aee1a
Bronco Oostermeyer <dev@bfv.io>
2022-11-18 10:02:06 +0100

    switched to fs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
     const filename = core.getInput('file');
     console.log(`attempt to parse: ${filename}`);
 
-    const file = new File(filename);
-    const exists = file.exists();
+    const exists = fs.existsSync(filename)
 
     console.log(`file '${filename}' found: ${exists}`);
-    
+
     core.setOutput('test-ok', true);
 }
 catch (error) {
